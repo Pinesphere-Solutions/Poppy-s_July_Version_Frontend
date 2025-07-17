ee842aa2ac9f058c79d0bd4357e07f110248c2d1
unknown <kirutika@pinesphere.com>
2025-07-17 18:19:23 +0530

    17.07

--- src-new/src/screens/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Dashboard } from "./dashboard/DashboardScreen";
-export { default as Reports } from "./dashboard/reports";
-export { default as Login } from "./Login";
-export { default as PageNotFound } from "./error/PageNotFound";
